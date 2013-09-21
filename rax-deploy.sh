2e64f0a34c02f190606350482b30ebcdda07700c
Constantine Peresypkin <constantine@litestack.com>
2013-09-21 08:26:20 -0500

    add more checks to rax-deploy.sh

--- rax-deploy.sh
+++ rax-deploy.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 ansible-playbook -i rax_inventory.py --private-key="$1" acme/create-chef-server.yaml
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 ansible-playbook -i rax_inventory.py --private-key="$1" acme/install-chef-server.yaml
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 ansible-playbook -i rax_inventory.py --private-key="$1" acme/create-servers.yaml
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 ansible-playbook -i rax_inventory.py --private-key="$1" acme/prepare-servers.yaml
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 ansible-playbook -i rax_inventory.py --private-key="$1" acme/chef-run.yaml
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 ansible-playbook -i rax_inventory.py --private-key="$1" acme/finalize-servers.yaml
