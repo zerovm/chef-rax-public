918eb6d8a72290b38dcdf8c1e2a51d38958ab2da
Constantine Peresypkin <constantine@litestack.com>
2013-09-21 08:33:10 -0500

    more check to deploy script

--- rax-deploy.sh
+++ rax-deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ -z "$1" ]; then
+    echo "Usage: $0 path_to_private_key"
+    exit 1
+fi
 ansible-playbook -i rax_inventory.py --private-key="$1" acme/create-chef-server.yaml
 if [ $? -ne 0 ]; then
     exit 1
